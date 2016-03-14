1eee1935aaa8c6ce5af9a1436cab911ead72eaaa
wangxiao <250455694@qq.com>
2016-03-14 15:41:26 +0800

    “添加剩下的控制器”

---
+++ Mercurial/FriendsIntroduceViewController.m
@@ -0,0 +1,47 @@
+//
+//  FriendsIntroduceViewController.m
+//  Mercurial
+//
+//  Created by 王霄 on 16/3/14.
+//  Copyright © 2016年 muggins. All rights reserved.
+//
+
+#import "FriendsIntroduceViewController.h"
+
+@interface FriendsIntroduceViewController ()
+
+@end
+
+@implementation FriendsIntroduceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationItem.title = @"熟人介绍";
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return 0;
+}
+
+
+
+@end
