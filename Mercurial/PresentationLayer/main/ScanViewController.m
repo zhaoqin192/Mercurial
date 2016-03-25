858eac024041fb8e75b058b54f25fa162b3d280a
wangxiao <250455694@qq.com>
2016-03-25 17:10:45 +0800

    完善产品扫描页面

--- Mercurial/PresentationLayer/main/ScanViewController.m
+++ Mercurial/PresentationLayer/main/ScanViewController.m
@@ -9,7 +9,10 @@
 #import "ScanViewController.h"
 
 @interface ScanViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *scanButton;
 
+@property (weak, nonatomic) IBOutlet UITextField *productTextField;
+@property (weak, nonatomic) IBOutlet UIButton *enquiryButton;
 @end
 
 @implementation ScanViewController
@@ -18,21 +21,29 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.navigationItem.title = @"防伪查询";
+    [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"menu_bg"]]];
+    [self configureButton];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)configureButton{
+    self.scanButton.backgroundColor = [UIColor clearColor];
+    self.scanButton.layer.cornerRadius = 8;
+    self.scanButton.layer.masksToBounds = YES;
+    self.scanButton.layer.borderWidth = 2;
+    self.scanButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    
+    self.enquiryButton.backgroundColor = [UIColor clearColor];
+    self.enquiryButton.layer.cornerRadius = 8;
+    self.enquiryButton.layer.masksToBounds = YES;
+    self.enquiryButton.layer.borderWidth = 2;
+    self.enquiryButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    
+    self.productTextField.layer.borderColor = [UIColor blueColor].CGColor;
+    self.productTextField.layer.borderWidth = 3;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
 }
-*/
 
 @end
