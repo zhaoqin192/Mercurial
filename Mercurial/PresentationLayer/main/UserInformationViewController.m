4fb248dfee6478985f401d8ce38b6dada841ebc5
wangxiao <250455694@qq.com>
2016-04-04 16:39:28 +0800

    完善个人信息选择功能

--- Mercurial/PresentationLayer/main/UserInformationViewController.m
+++ Mercurial/PresentationLayer/main/UserInformationViewController.m
@@ -8,9 +8,12 @@
 
 #import "UserInformationViewController.h"
 #import "ActionSheetStringPicker.h"
+#import "ActionSheetDatePicker.h"
+#import "Account.h"
+#import "AccountDao.h"
 
 @interface UserInformationViewController ()
-
+@property (nonatomic, strong) Account *myAccount;
 @end
 
 @implementation UserInformationViewController
@@ -18,18 +21,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = @"用户信息";
+    [self loadData];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)loadData{
+    [NetworkRequest requestUserInformationWithToken:^{
+        self.myAccount = [[[AccountDao alloc] init] getAccount];
+        [self.tableView reloadData];
+    } failure:^{
+        [SVProgressHUD showErrorWithStatus:@"获取数据失败，请重新尝试"];
+        [self performSelector:@selector(dismiss) withObject:nil afterDelay:0.5f];
+    }];
+}
+
+- (void)dismiss {
+    [SVProgressHUD dismiss];
 }
 
 #pragma mark - Table view data source
 
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 13;
+    return 15;
 }
 
 
@@ -38,62 +51,79 @@
     
     switch (indexPath.row) {
         case 0:{
-            cell.textLabel.text = @"登录用户";
-            cell.detailTextLabel.text = @"xxxx";
+            cell.textLabel.text = @"用户名";
+            cell.detailTextLabel.text = self.myAccount.accountName;
+            cell.selectionStyle = UITableViewCellSelectionStyleNone;
             break;
         }
         case 1:
             cell.textLabel.text = @"真实姓名";
-            cell.detailTextLabel.text = @"xxxx";
+            cell.detailTextLabel.text = self.myAccount.name;
             break;
         case 2:
             cell.textLabel.text = @"性别";
-            cell.detailTextLabel.text = @"xxxx";
+            cell.detailTextLabel.text = self.myAccount.sex;
             break;
         case 3:
-            cell.textLabel.text = @"出生日期";
-            cell.detailTextLabel.text = @"xxxx";
+            cell.textLabel.text = @"年龄";
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%@",self.myAccount.age];
             break;
         case 4:
-            cell.textLabel.text = @"身份证号";
-            cell.detailTextLabel.text = @"xxxx";
+            cell.textLabel.text = @"出生日期";
+            cell.detailTextLabel.text = self.myAccount.birth;
             break;
         case 5:
-            cell.textLabel.text = @"固定电话";
-            cell.detailTextLabel.text = @"xxxx";
+            cell.textLabel.text = @"身份证号";
+            cell.detailTextLabel.text = self.myAccount.cardID;
             break;
         case 6:
-            cell.textLabel.text = @"邮箱";
-            cell.detailTextLabel.text = @"xxxx";
+            cell.textLabel.text = @"固定电话";
+            cell.detailTextLabel.text = self.myAccount.phone;
             break;
         case 7:
-            cell.textLabel.text = @"职业";
-            cell.detailTextLabel.text = @"xxxx";
+            cell.textLabel.text = @"邮箱";
+            cell.detailTextLabel.text = self.myAccount.email;
             break;
         case 8:
-            cell.textLabel.text = @"学历";
-            cell.detailTextLabel.text = @"xxxx";
+            cell.textLabel.text = @"职业";
+            cell.detailTextLabel.text = self.myAccount.job;
             break;
         case 9:
-            cell.textLabel.text = @"省";
-            cell.detailTextLabel.text = @"xxxx";
+            cell.textLabel.text = @"学历";
+            cell.detailTextLabel.text = self.myAccount.degree;
             break;
         case 10:
-            cell.textLabel.text = @"市";
-            cell.detailTextLabel.text = @"xxxx";
+            cell.textLabel.text = @"详细地址";
+            cell.detailTextLabel.text = self.myAccount.address;
             break;
         case 11:
-            cell.textLabel.text = @"区县";
-            cell.detailTextLabel.text = @"xxxx";
+            cell.textLabel.text = @"装修经验";
+            cell.detailTextLabel.text = self.myAccount.experience;
             break;
         case 12:
-            cell.textLabel.text = @"地址";
-            cell.detailTextLabel.text = @"xxxx";
+            cell.textLabel.text = @"了解渠道";
+            cell.detailTextLabel.text = self.myAccount.way;
+            break;
+        case 13:
+            cell.textLabel.text = @"是否购买过本品牌";
+            NSLog(@"%@",self.myAccount.isBought);
+            if ([self.myAccount.isBought  isEqual: @(1)]) {
+                cell.detailTextLabel.text = @"是";
+            }
+            else{
+                cell.detailTextLabel.text = @"否";
+            }
+            break;
+        case 14:
+            cell.textLabel.text = @"购买过的品牌";
+            cell.detailTextLabel.text = self.myAccount.brand;
             break;
         default:
             break;
     }
-    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    if(indexPath.row != 0){
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
     [cell layoutIfNeeded];
     return cell;
 }
@@ -104,10 +134,53 @@
     switch (indexPath.row) {
         case 2:{
             [ActionSheetStringPicker showPickerWithTitle:@"请选择性别" rows:@[@[@"男", @"女", @"未知"]] initialSelection:@[@(0)] doneBlock:^(ActionSheetStringPicker *picker, NSArray * selectedIndex, NSArray *selectedValue) {
-                UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-                cell.detailTextLabel.text = [selectedValue firstObject];
-                //[weakSelf.tableView reloadData];
-                //改数据源 发网络请求  更新数据而不是改cell
+                self.myAccount.sex = [selectedValue firstObject];
+                [weakSelf.tableView reloadData];
+            } cancelBlock:nil origin:self.view];
+            break;
+        }
+        case 4:{
+            NSDate *curDate = [NSDate dateFromString:@"1990-01-01" withFormat:@"yyyy-MM-dd"];            ActionSheetDatePicker *picker = [[ActionSheetDatePicker alloc] initWithTitle:nil datePickerMode:UIDatePickerModeDate selectedDate:curDate doneBlock:^(ActionSheetDatePicker *picker, NSDate *selectedDate, id origin) {
+                weakSelf.myAccount.birth = [selectedDate string_yyyy_MM_dd];
+                [weakSelf.tableView reloadData];
+            } cancelBlock:^(ActionSheetDatePicker *picker) {
+
+            } origin:self.view];
+            picker.minimumDate = [[NSDate date] offsetYear:-120];
+            picker.maximumDate = [NSDate date];
+            [picker showActionSheetPicker];
+            break;
+        }
+        case 8:{
+            [ActionSheetStringPicker showPickerWithTitle:@"请选择职业" rows:@[@[@"机关事业单位从业人员", @"职业经理人", @"销售人员",@"财务工作人员",@"行政内勤人员",@"医生或教师",@"从商人员（包括个体户、私营企业主等）",@"专项领域技术人员",@"媒体工作者",@"艺术或文学工作者",@"服务型人员",@"服役军人",@"学生、退休人员",@"自由职业"]] initialSelection:@[@(0)] doneBlock:^(ActionSheetStringPicker *picker, NSArray * selectedIndex, NSArray *selectedValue) {
+                self.myAccount.job = [selectedValue firstObject];
+                [weakSelf.tableView reloadData];
+            } cancelBlock:nil origin:self.view];
+            break;
+        }
+        case 9:{
+            [ActionSheetStringPicker showPickerWithTitle:@"请选择学历" rows:@[@[@"小学",@"初中",@"高中",@"中专",@"大专",@"本科",@"研究生",@"博士",@"博士后"]] initialSelection:@[@(0)] doneBlock:^(ActionSheetStringPicker *picker, NSArray * selectedIndex, NSArray *selectedValue) {
+                self.myAccount.degree = [selectedValue firstObject];
+                [weakSelf.tableView reloadData];
+            } cancelBlock:nil origin:self.view];
+            break;
+        }
+        case 13:{
+            [ActionSheetStringPicker showPickerWithTitle:@"是否购买过本品牌" rows:@[@[@"是",@"否"]] initialSelection:@[@(0)] doneBlock:^(ActionSheetStringPicker *picker, NSArray * selectedIndex, NSArray *selectedValue) {
+                if ([[selectedValue firstObject] isEqualToString:@"是"]) {
+                    self.myAccount.isBought = @(YES);
+                }
+                else{
+                    self.myAccount.isBought = @(NO);
+                }
+                [weakSelf.tableView reloadData];
+            } cancelBlock:nil origin:self.view];
+            break;
+        }
+        case 14:{
+            [ActionSheetStringPicker showPickerWithTitle:@"请选择购买过的品牌" rows:@[@[@"马可波罗",@"唯美"]] initialSelection:@[@(0)] doneBlock:^(ActionSheetStringPicker *picker, NSArray * selectedIndex, NSArray *selectedValue) {
+                self.myAccount.brand = [selectedValue firstObject];
+                [weakSelf.tableView reloadData];
             } cancelBlock:nil origin:self.view];
             break;
         }
