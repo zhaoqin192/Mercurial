4205b1eb0dc903e2a749f090d7eeff585f0328d7
wangxiao <250455694@qq.com>
2016-04-06 21:26:13 +0800

    用户推荐页面

--- Mercurial/PresentationLayer/main/FriendsIntroduceViewController.m
+++ Mercurial/PresentationLayer/main/FriendsIntroduceViewController.m
@@ -2,46 +2,149 @@
 //  FriendsIntroduceViewController.m
 //  Mercurial
 //
-//  Created by 王霄 on 16/3/14.
+//  Created by 王霄 on 16/4/6.
 //  Copyright © 2016年 muggins. All rights reserved.
 //
 
 #import "FriendsIntroduceViewController.h"
+#import "Account.h"
 
 @interface FriendsIntroduceViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *nameTF;
+@property (weak, nonatomic) IBOutlet UITextField *phoneTF;
+@property (weak, nonatomic) IBOutlet UITextField *addressTF;
+@property (weak, nonatomic) IBOutlet UITextField *productTF;
+@property (weak, nonatomic) IBOutlet UITextField *reasonTF;
+@property (weak, nonatomic) IBOutlet UIButton *saveButton;
+@property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (nonatomic, strong) Account *myAccount;
 @end
 
 @implementation FriendsIntroduceViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.navigationItem.title = @"用户推荐";
+    [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"menu_bg"]]];
+    [self configureRegisterButton];
+    [self.nameTF becomeFirstResponder];
+    [SVProgressHUD setDefaultStyle:SVProgressHUDStyleDark];
+    [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeClear];
+}
+
+- (void)configureRegisterButton{
+    self.saveButton.backgroundColor = [UIColor clearColor];
+    self.saveButton.layer.cornerRadius = 8;
+    self.saveButton.layer.masksToBounds = YES;
+    self.saveButton.layer.borderWidth = 2;
+    self.saveButton.layer.borderColor = [UIColor whiteColor].CGColor;
     
-    self.navigationItem.title = @"熟人介绍";
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    self.backButton.backgroundColor = [UIColor clearColor];
+    self.backButton.layer.cornerRadius = 8;
+    self.backButton.layer.masksToBounds = YES;
+    self.backButton.layer.borderWidth = 2;
+    self.backButton.layer.borderColor = [UIColor whiteColor].CGColor;
+}
+
+- (void)dismiss {
+    [SVProgressHUD dismiss];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
+
+#pragma mark -<VerficationCode>
+
+- (BOOL)isValidPhoneNumber:(NSString *)text{
+    if (text.length != 11) {
+        return NO;
+    }else if (![self isAllNum:text]){
+        return NO;
+    }else{
+        return YES;
+    }
+}
+
+- (BOOL)isValidAgeNumber:(NSString *)text{
+    if (!(text.length == 1 || text.length == 2)) {
+        return NO;
+    }else if (![self isAllNum:text]){
+        return NO;
+    }else{
+        return YES;
+    }
+}
+
+- (BOOL)isAllNum:(NSString *)text{
+    unichar c;
+    for (int i=0; i<text.length; i++) {
+        c=[text characterAtIndex:i];
+        if (!isdigit(c)) {
+            return NO;
+        }
+    }
+    return YES;
+}
+- (IBAction)saveButtonClicked {
+    if(![self isValidPhoneNumber:self.phoneTF.text]){
+        [SVProgressHUD showErrorWithStatus:@"请输入正确的推荐人电话号码"];
+        [self performSelector:@selector(dismiss) withObject:nil afterDelay:0.5f];
+        return;
+    }
+    if (self.nameTF.text.length == 0) {
+        [SVProgressHUD showErrorWithStatus:@"请输入推荐人姓名"];
+        [self performSelector:@selector(dismiss) withObject:nil afterDelay:0.5f];
+        return;
+    }
+    if (self.addressTF.text.length == 0) {
+        [SVProgressHUD showErrorWithStatus:@"请输入推荐人地址"];
+        [self performSelector:@selector(dismiss) withObject:nil afterDelay:0.5f];
+        return;
+    }
+    if (self.productTF.text.length == 0) {
+        [SVProgressHUD showErrorWithStatus:@"请输入推荐产品"];
+        [self performSelector:@selector(dismiss) withObject:nil afterDelay:0.5f];
+        return;
+    }
+    if (self.reasonTF.text.length == 0) {
+        [SVProgressHUD showErrorWithStatus:@"请输入推荐理由"];
+        [self performSelector:@selector(dismiss) withObject:nil afterDelay:0.5f];
+        return;
+    }
+    [SVProgressHUD show];
+    self.myAccount = [[[AccountDao alloc] init] getAccount];
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    NSDate *date1 = [NSDate date];
+    //然后您需要定义一个NSDataFormat的对象
+    NSDateFormatter * dateFormat = [[NSDateFormatter alloc]init];
+    //然后设置这个类的dataFormate属性为一个字符串，系统就可以因此自动识别年月日时间
+    dateFormat.dateFormat = @"yyyy年MM月dd日 HH:mm:SS";
+    //之后定义一个字符串，使用stringFromDate方法将日期转换为字符串
+    NSString * dateToString = [dateFormat stringFromDate:date1];
+    
+    [NetworkRequest requestAddCommend:self.nameTF.text phone:self.phoneTF.text province:[self notNil:self.myAccount.province] city:[self notNil:self.myAccount.city] district:[self notNil:self.myAccount.district] address:self.addressTF.text commendName:self.productTF.text date:dateToString reason:self.reasonTF.text success:^{
+        [SVProgressHUD showSuccessWithStatus:@"推荐成功!"];
+        [self performSelector:@selector(dismiss) withObject:nil afterDelay:0.5f];
+        [self.navigationController popViewControllerAnimated:YES];
+    } failure:^{
+        [SVProgressHUD showErrorWithStatus:@"推荐失败"];
+        [self performSelector:@selector(dismiss) withObject:nil afterDelay:0.5f];
+    }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)backButtonClicked {
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+- (NSString *)notNil:(NSString *)text{
+    if (text) {
+        return text;
+    }
+    else{
+        return @"";
+    }
 }
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
-}
-
-
 
 @end
