fbec9df152b31fe36c4a994c2770ab454d4c55af
wangxiao <wangxiao@ebupt.com>
2016-06-01 20:47:09 +0800

    添加论坛搜索功能

--- Mercurial/PresentationLayer/main/FormViewController.m
+++ Mercurial/PresentationLayer/main/FormViewController.m
@@ -17,9 +17,16 @@
 #import "DatabaseManager.h"
 #import "AccountDao.h"
 #import "LoginViewController.h"
+#import "MJExtension.h"
+#import "DatabaseManager.h"
+#import "Account.h"
+#import "AccountDao.h"
+#import "NetworkManager.h"
 
-@interface FormViewController ()
+@interface FormViewController () <UIScrollViewDelegate,UISearchBarDelegate>
 @property (nonatomic, copy) NSArray *list;
+@property (nonatomic, copy) NSArray *searchResults;
+@property (nonatomic, strong) UISearchBar *bar;
 @end
 
 @implementation FormViewController
@@ -27,6 +34,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self configureNavigationItem];
+    self.tableView.tableHeaderView = ({
+        self.bar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, KScreen_width, 40)];
+        self.bar.delegate = self;
+        self.bar;
+    });
     [self.tableView registerNib:[UINib nibWithNibName:@"TopicCell" bundle:nil] forCellReuseIdentifier:@"TopicCell"];
     self.tableView.rowHeight = 90;
 }
@@ -147,22 +159,65 @@
 
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.list.count;
+    if (self.bar.isFirstResponder) {
+        return self.searchResults.count;
+    }
+    else {
+        return self.list.count;
+    }
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     TopicCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"TopicCell"];
-    Topic *topic = self.list[indexPath.row];
+    Topic *topic = nil;
+    if (self.bar.isFirstResponder) {
+        topic = self.searchResults[indexPath.row];
+    }
+    else {
+        topic = self.list[indexPath.row];
+    }
     cell.topic = topic;
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    Topic *topic = self.list[indexPath.row];
+    Topic *topic = nil;
+    if (self.bar.isFirstResponder) {
+        topic = self.searchResults[indexPath.row];
+    }
+    else {
+        topic = self.list[indexPath.row];
+    }
     FormDetailViewController *vc = [[FormDetailViewController alloc] init];
     vc.topic_id = topic.topic_id;
     [self.navigationController pushViewController:vc animated:YES];
+}
+
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    [self.view endEditing:YES];
+    [self.tableView reloadData];
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    AFHTTPSessionManager *manager = [[NetworkManager sharedInstance] getRequestQueue];
+    NSURL *URL = [NSURL URLWithString:[URLPREFIX stringByAppendingString:@"/weimei_background/index.php/Forum/Index/search_topic"]];
+    Account *account = [[DatabaseManager sharedAccount] getAccount];
+    NSDictionary *parames = @{@"sid": account.token, @"main_title": searchBar.text};
+    [manager POST:URL.absoluteString parameters:parames progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"%@", responseObject);
+        if([[responseObject objectForKey:@"status"] isEqualToString:@"200"]){
+            self.searchResults = [Topic mj_objectArrayWithKeyValuesArray:responseObject[@"data"]];
+            [self.tableView reloadData];
+        }
+        else{
+            [SVProgressHUD showErrorWithStatus:@"加载数据失败"];
+            [self performSelector:@selector(dismiss) withObject:nil afterDelay:1.5f];
+        }
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"%@", error);
+    }];
 }
 
 
