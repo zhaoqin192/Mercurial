4564fd513da938271d1bb4c2fadbe916f07ebf5b
zhaoqin192 <zhaoqin192@gmail.com>
2016-06-02 19:59:42 +0800

    add Weixin SDK

--- Mercurial/BusinessLogicLayer/Utility/Utility.m
+++ Mercurial/BusinessLogicLayer/Utility/Utility.m
@@ -14,7 +14,7 @@
 + (NSString *)md5:(NSString *)input{
     const char *cStr = [input UTF8String];
     unsigned char result[16];
-    CC_MD5(cStr, strlen(cStr), result); // This is the md5 call
+    CC_MD5(cStr, (CC_LONG)strlen(cStr), result); // This is the md5 call
     return [NSString stringWithFormat:
             @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
             result[0], result[1], result[2], result[3],
